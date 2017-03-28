45202c994799c6b38c2b07f05447bcb71cd45560
greg <greg.lowe.v1@gmail.com>
2017-03-28 21:55:57 +1300

    changes

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -51,65 +51,62 @@ namespace GildedRose.Console
                 if (item.Quality < 50)
                 {
                     item.Quality = item.Quality + 1;
-
-
+                }
+                item.SellIn = item.SellIn - 1;
+                if (item.SellIn < 0)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
                 }
             }
             else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Quality < 50)
+
+                if (item.SellIn < 6 && item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 3;
+                }
+                else if (item.SellIn < 11 && item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 2;
+                }
+                else if (item.Quality < 50)
                 {
                     item.Quality = item.Quality + 1;
-
-                    if (item.SellIn < 11)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-
                 }
-            }
-            else
-            {
-                
-                    if (item.Name == "Sulfuras, Hand of Ragnaros")
-                    { }
-                    else  if (item.Quality > 0)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                
-            }
+                item.SellIn = item.SellIn - 1;
 
-            if (item.Name == "Sulfuras, Hand of Ragnaros")
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+
+            }
+            else if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
 
             }
-            else
+            else if (item.Name == "Conjured Mana Cake")
             {
+                if (item.Quality > 0)
+                {
+                    item.Quality = item.Quality - 1;
+                    item.SellIn = item.SellIn - 1;
+                }
+
+            }
+            else if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
                 item.SellIn = item.SellIn - 1;
             }
 
-            if (item.Name == "Aged Brie" && item.SellIn < 0)
+
+             if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 0)
             {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
-            }
-            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 0)
-            {
-                item.Quality = item.Quality - item.Quality;
+               
             }
             else if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
